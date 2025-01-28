4e7e854d292c59f10b54d20825f9ae7964cbe00b
Alexander Ramirez <Alexander.Ramirez@staples.ca>
2025-01-28 10:44:10 -0600

    Innitial commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -2,6 +2,7 @@ package org.acme;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.OPTIONS;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Produces;
@@ -30,6 +31,19 @@ public class GreetingResource {
                                      .add("message", "Hello " + name)
                                      .build();
         return Response.ok(responseJson)
+                       .header("Access-Control-Allow-Origin", "*")
+                       .header("Access-Control-Allow-Credentials", "true")
+                       .header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS")
+                       .header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+                       .header("Vary", "Origin")
+                       .build();
+    }
+
+    @OPTIONS
+    @Path("/hellomessage")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response optionsHelloMessage() {
+        return Response.ok()
                        .header("Access-Control-Allow-Origin", "*")
                        .header("Access-Control-Allow-Credentials", "true")
                        .header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS")
